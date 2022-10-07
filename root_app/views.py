a3e742c0b4ed58717935221904ab805e25464598
MashodRana <mashod0rana@gmail.com>
2022-10-07 10:23:56 +0600

    home page view

--- root_app/views.py
+++ root_app/views.py
@@ -1,3 +1,11 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class HomeView(View):
+    template_name = 'pages/home_page.html'
+    def get(self, request, *args, **kwargs):
+        
+        return render(request, self.template_name, context={"name":"mashod"})
