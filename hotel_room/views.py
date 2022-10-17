d5a4aa5e9a0263115263f87ea2aad97f4f925fee
MashodRana <mashod0rana@gmail.com>
2022-10-17 10:13:27 +0600

    room context data passed to templates

--- hotel_room/views.py
+++ hotel_room/views.py
@@ -1,9 +1,12 @@
+from re import I
 from django.shortcuts import render
 from django.views import View
+from .models import Room
 
 # Create your views here.
 class RoomsView(View):
     template_name = 'pages/rooms.html'
 
     def get(self, req, *args, **kwargs):
-        return render(req, self.template_name, context={'rooms':'All romms'})
+        rooms = Room.objects.all()
+        return render(req, self.template_name, context={'rooms':rooms})
