a627432dec6313b07a3aed00b7c0358b261138a8
MashodRana <mashod0rana@gmail.com>
2022-11-28 21:27:53 +0600

    wishlist models

--- room_booking/admin.py
+++ room_booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from room_booking.models import Booking
+from room_booking.models import Booking, WishList
 
 
 # Register your models here.
 admin.site.register(Booking)
+admin.site.register(WishList)
--- room_booking/models.py
+++ room_booking/models.py
@@ -28,4 +28,19 @@ class Booking(models.Model):
     breakfast = models.BooleanField(default=False)
 
 class WishList(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    check_in = models.DateTimeField(blank=True, null=True)
+    check_out = models.DateTimeField(blank=True, null=True)
+    room_cleaning = models.BooleanField(default=False)
+    laundry = models.BooleanField(default=False)
+    breakfast = models.BooleanField(default=False)
+    total_price = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.room.room_number} {self.room.style}"
+
+
+
