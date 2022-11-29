a5f13f0d3cdcfcbfed3f589c31bca93124e9e7ca
MashodRana <mashod0rana@gmail.com>
2022-11-29 11:19:31 +0600

    adding to wishlist

--- room_booking/urls.py
+++ room_booking/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
 
 from room_booking import views
 
 
 urlpatterns = [
-    path('add-to-wishlist/', login_required(views.WishListView.as_view()), name='add_to_wishlist'),
+    path('add-to-wishlist/', views.WishListView.as_view(), name='add_to_wishlist'),
 
 ]
--- room_booking/views.py
+++ room_booking/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class WishListView(View):
+class WishListView(LoginRequiredMixin, View):
     __template_name = 'room_booking/wishlist.html'
+    # login_url = ''
 
     def post(self, request, *args, **kwargs):
         print(request)
         print(request.user)
-        return HttpResponse("User Profile View")
+        return JsonResponse({"msg": "Added to wish list."})
     def get(self, request, *args, **kwargs):
         pass
