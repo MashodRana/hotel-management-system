0484bb57c7bea056b0ed7cfa0b9e061a1669049f
MashodRana <mashod0rana@gmail.com>
2022-12-04 20:21:05 +0600

    url added for my-booking

--- room_booking/urls.py
+++ room_booking/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add-to-wishlist/', views.AddToWishListView.as_view(), name='add_to_wishlist'),
     path('wishlist/', views.WishListView.as_view(), name='wishlist'),
     path('wishlist/<int:id>', views.DeleteWishListItemView.as_view(), name='delete_wishlist_item'),
+    path('my-bookings', views.BookingListView.as_view(), name='my_bookings'),
 
 ]
