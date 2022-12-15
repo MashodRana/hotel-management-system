79c5242a7e9a30a9ae581408b9d870c6f0248c91
MashodRana <mashod0rana@gmail.com>
2022-12-15 07:10:56 +0600

    sear room view udpated.

--- root_app/views.py
+++ root_app/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 import json
 
 from root_app.forms import SearchForm
+from hotel_room.models import Room
+from room_booking.models import BookingHistory
 
 
 # Create your views here.
@@ -19,13 +21,29 @@ class HomeView(View):
         return render(request, self.template_name, context=context)
 
 class SearchRoomView(View):
-    __template_name = 'pages/search_result.html'
+    # __template_name = 'pages/search_result.html'
+    __template_name ='pages/rooms.html'
+
 
     def get(self, request, *args, **kwargs):
-        print(request.GET)
         form = SearchForm(request.GET)
         if form.is_valid():
-            print("uer valid form")
+            type = form.cleaned_data.get('room_type')
+            check_out = form.cleaned_data.get('check_out')
+            check_in = form.cleaned_data.get('check_in')
+            to_ingnore = BookingHistory.objects.filter(
+                check_in__lt=check_out, 
+                check_out__gt=check_in, 
+                room__style=type).values('room')
+
+            rooms = Room.objects.all().exclude(id__in=to_ingnore)
+            # Room.objects.filter(~Q(id__in=to_ingnore))
+            context = {
+                'title': 'Search Result',
+                'active_nav_item': 'nav-rooms',
+                'rooms':rooms
+            }
+            return render(request=request, template_name=self.__template_name, context=context)            
         else:
             context = {
                 'title': 'MR Palace',
